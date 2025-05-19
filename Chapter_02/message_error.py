b6fa054bc1ef98d8cec71209ccd5a2bb81762c8e
Braian Soares Mesquita <b.mesquita.data@gmail.com>
2025-05-18 23:11:00 -0300

    Initial commit: clean Python Crash Course structure

---
+++ Chapter_02/message_error.py
@@ -0,0 +1,7 @@
+# This program contais an intentional variable error to test how the Python interpreter
+# handles error messages. This is for learning purposes, as suggested in the book.
+message = "Hello Python Crash Course reader!"
+print(mesage) # intentional error: variable 'mesage' is not defined
+ 
+# Expected output (error):
+NameError: name 'mesage' is not defined
