76450d4a6aa0a4911f81e797f42260fab5d65004
Braian Soares Mesquita <b.mesquita.data@gmail.com>
2025-05-19 23:32:27 -0300

    Add name.py demonstrating the use of title() to capitalize the first letter of each word in a name

---
+++ Chapter_02/name.py
@@ -0,0 +1,4 @@
+# Using the title() function in Python to capitalize the first letter of each word in a string
+name = 'ada lovelace'
+print(name.title())
+# Output: 'Ada Lovelace' - Formats the name with title case
