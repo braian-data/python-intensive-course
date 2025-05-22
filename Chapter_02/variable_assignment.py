441a830e975b523a4b92876e4391b4026d4a6c70
Braian Soares Mesquita <b.mesquita.data@gmail.com>
2025-05-22 01:25:58 -0300

    Update variable_assignment.py

--- Chapter_02/variable_assignment.py
+++ Chapter_02/variable_assignment.py
@@ -1,1 +1,7 @@
-print (message)
+# Assigning a string value to the variable 'message'
+message = "Hello Python world!"
+print(message)
+
+# Reassigning a new string value to the same variable
+message = "Hello Python Crash Course world!"
+print(message)
